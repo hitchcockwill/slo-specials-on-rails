ac57395e3d87ca000c7f2ca61f94146a278ebb57
Will Hitchcock <hitchcock.will@gmail.com>
2012-10-19 13:35:44 -0700

    loaded rails

---
+++ slo-specials/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SloSpecials::Application.config.secret_token = '44a2b061b3400a450fef325fd878caf7d8f10f70ebeffb5b47a41c0ba5cd0e50eef84703be388af304839a6874cc92fba66e2ff20d8a756e8fcc55b294d74056'
