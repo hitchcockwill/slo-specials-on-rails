d8c863aca7bb80ecc889109cccb0f293c0990dbf
Will Hitchcock <hitchcock.will@gmail.com>
2012-10-19 14:39:03 -0700

    created deal scaffold

--- slo-specials/app/models/venue.rb
+++ slo-specials/app/models/venue.rb
@@ -1,4 +1,11 @@
 class Venue
   include Mongoid::Document
+  
   field :name, type: String
+
+  validates :name, :presence => :true
+
+  attributes_accessible :name
+
+  has_many :deals
 end
--- slo-specials/config/routes.rb
+++ slo-specials/config/routes.rb
@@ -1,4 +1,6 @@
 SloSpecials::Application.routes.draw do
+  resources :deals
+
   resources :venues
 
   # The priority is based upon order of creation:
