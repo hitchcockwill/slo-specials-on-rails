82bf30b10121e12897ed43077cadf70297aa0dcc
Will Hitchcock <hitchcock.will@gmail.com>
2012-10-20 16:58:16 -0700

    got venues and deals in the database

--- slo-specials/app/models/deal.rb
+++ slo-specials/app/models/deal.rb
@@ -3,13 +3,15 @@ class Deal
 
   field :title
   field :kind
+  field :unit
   field :price, type: Float
+  field :time, type: Hash
 
   validates :title, :presence => :true
   validates :kind, :presence => :true
   validates :price, :presence => :true
 
-  attr_accessible :title, :kind, :price, :venue_id
+  attr_accessible :title, :kind, :price, :venue_id, :unit, :time, :details
 
   belongs_to :venue
 end
